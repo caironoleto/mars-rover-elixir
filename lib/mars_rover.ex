d5f6dea117bcbcc25e7f8cd399e156e7a606b06e
Cairo Noleto <caironoleto@gmail.com>
2019-06-13 10:55:44 -0300

    Left move completed

--- lib/mars_rover.ex
+++ lib/mars_rover.ex
@@ -1,18 +1,31 @@
 defmodule MarsRover do
-  @moduledoc """
-  Documentation for MarsRover.
-  """
+  defstruct [:x, :y, :face]
 
-  @doc """
-  Hello world.
+  def add_rover(x, y, face) do
+    %MarsRover{x: x, y: y, face: face}
+  end
 
-  ## Examples
+  def send_command(rover = %MarsRover{}, commands) do
+    commands = String.split(commands, ~r{b}, include_captures: true)
 
-      iex> MarsRover.hello()
-      :world
+    do_send_command(rover, commands)
+  end
 
-  """
-  def hello do
-    :world
+  defp do_send_command(rover = %MarsRover{}, []), do: rover
+  defp do_send_command(%MarsRover{x: x, y: y, face: "N"}, ["L" | next_commands]) do
+    %MarsRover{y: y, x: x, face: "W"}
+    |> do_send_command(next_commands)
+  end
+  defp do_send_command(%MarsRover{x: x, y: y, face: "W"}, ["L" | next_commands]) do
+    %MarsRover{y: y, x: x, face: "S"}
+    |> do_send_command(next_commands)
+  end
+  defp do_send_command(%MarsRover{x: x, y: y, face: "S"}, ["L" | next_commands]) do
+    %MarsRover{y: y, x: x, face: "E"}
+    |> do_send_command(next_commands)
+  end
+  defp do_send_command(%MarsRover{x: x, y: y, face: "E"}, ["L" | next_commands]) do
+    %MarsRover{y: y, x: x, face: "N"}
+    |> do_send_command(next_commands)
   end
 end
--- test/mars_rover_test.exs
+++ test/mars_rover_test.exs
@@ -1,8 +1,32 @@
 defmodule MarsRoverTest do
   use ExUnit.Case
-  doctest MarsRover
 
-  test "greets the world" do
-    assert MarsRover.hello() == :world
+  test "add_rover/3 returns a MarsRover struct with the position" do
+    assert %MarsRover{x: 1, y: 2, face: "N"} = MarsRover.add_rover(1, 2, "N")
+    assert %MarsRover{x: 3, y: 3, face: "E"} = MarsRover.add_rover(3, 3, "E")
+  end
+
+  test "send_command/2 rotate the rover to West when the face is North and the command is to turn Left" do
+    rover = MarsRover.add_rover(1, 2, "N")
+
+    assert %MarsRover{x: 1, y: 2, face: "W"} = MarsRover.send_command(rover, "L")
+  end
+
+  test "send_command/2 rotate the rover to South when the face is West and the command is to turn Left" do
+    rover = MarsRover.add_rover(1, 2, "W")
+
+    assert %MarsRover{x: 1, y: 2, face: "S"} = MarsRover.send_command(rover, "L")
+  end
+
+  test "send_command/2 rotate the rover to East when the face is South and the command is to turn Left" do
+    rover = MarsRover.add_rover(1, 2, "S")
+
+    assert %MarsRover{x: 1, y: 2, face: "E"} = MarsRover.send_command(rover, "L")
+  end
+
+  test "send_command/2 rotate the rover to North when the face is East and the command is to turn Left" do
+    rover = MarsRover.add_rover(1, 2, "E")
+
+    assert %MarsRover{x: 1, y: 2, face: "N"} = MarsRover.send_command(rover, "L")
   end
 end
