09593fa53973b316e53f903e2b8e395c42927b06
Cairo Noleto <caironoleto@gmail.com>
2019-06-13 11:48:42 -0300

    Remove function guard clause and use pattern matching

--- lib/mars_rover.ex
+++ lib/mars_rover.ex
@@ -13,47 +13,42 @@ defmodule MarsRover do
 
   defp do_send_command(rover = %MarsRover{}, []), do: rover
 
-  defp do_send_command(%MarsRover{x: x, y: y, face: face}, [command | next_commands])
-       when command == "L" do
+  defp do_send_command(%MarsRover{x: x, y: y, face: face}, ["L" | next_commands]) do
     face
     |> rotate_left()
     |> build_rover(x, y)
     |> do_send_command(next_commands)
   end
 
-  defp do_send_command(%MarsRover{x: x, y: y, face: face}, [command | next_commands])
-       when command == "R" do
+  defp do_send_command(%MarsRover{x: x, y: y, face: face}, ["R" | next_commands]) do
     face
     |> rotate_right()
     |> build_rover(x, y)
     |> do_send_command(next_commands)
   end
 
-  defp do_send_command(%MarsRover{x: x, y: y, face: "N"}, [command | next_commands])
-       when command == "M" do
+  defp do_send_command(%MarsRover{x: x, y: y, face: "N"}, ["M" | next_commands]) do
     build_rover("N", x, y + 1)
     |> do_send_command(next_commands)
   end
 
-  defp do_send_command(%MarsRover{x: x, y: y, face: "E"}, [command | next_commands])
-       when command == "M" do
+  defp do_send_command(%MarsRover{x: x, y: y, face: "E"}, ["M" | next_commands]) do
     build_rover("E", x + 1, y)
     |> do_send_command(next_commands)
   end
 
-  defp do_send_command(%MarsRover{x: x, y: y, face: "S"}, [command | next_commands])
-       when command == "M" do
+  defp do_send_command(%MarsRover{x: x, y: y, face: "S"}, ["M" | next_commands]) do
     build_rover("S", x, y - 1)
     |> do_send_command(next_commands)
   end
 
-  defp do_send_command(%MarsRover{x: x, y: y, face: "W"}, [command | next_commands])
-       when command == "M" do
+  defp do_send_command(%MarsRover{x: x, y: y, face: "W"}, ["M" | next_commands]) do
     build_rover("W", x - 1, y)
     |> do_send_command(next_commands)
   end
 
   defp build_rover(face, x, y), do: %MarsRover{x: x, y: y, face: face}
+
   defp rotate_left(face) when face == "N", do: "W"
   defp rotate_left(face) when face == "W", do: "S"
   defp rotate_left(face) when face == "S", do: "E"
