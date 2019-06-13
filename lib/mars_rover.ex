c787de95157e52ea47a3d7a6b48c98d0894fa3f4
Cairo Noleto <caironoleto@gmail.com>
2019-06-13 11:16:43 -0300

    Add private function to build the Mars Rover struct

--- lib/mars_rover.ex
+++ lib/mars_rover.ex
@@ -13,36 +13,38 @@ defmodule MarsRover do
 
   defp do_send_command(rover = %MarsRover{}, []), do: rover
   defp do_send_command(%MarsRover{x: x, y: y, face: "N"}, ["L" | next_commands]) do
-    %MarsRover{y: y, x: x, face: "W"}
+    build_rover(x, y, "W")
     |> do_send_command(next_commands)
   end
   defp do_send_command(%MarsRover{x: x, y: y, face: "W"}, ["L" | next_commands]) do
-    %MarsRover{y: y, x: x, face: "S"}
+    build_rover(x, y, "S")
     |> do_send_command(next_commands)
   end
   defp do_send_command(%MarsRover{x: x, y: y, face: "S"}, ["L" | next_commands]) do
-    %MarsRover{y: y, x: x, face: "E"}
+    build_rover(x, y, "E")
     |> do_send_command(next_commands)
   end
   defp do_send_command(%MarsRover{x: x, y: y, face: "E"}, ["L" | next_commands]) do
-    %MarsRover{y: y, x: x, face: "N"}
+    build_rover(x, y, "N")
     |> do_send_command(next_commands)
   end
 
   defp do_send_command(%MarsRover{x: x, y: y, face: "W"}, ["R" | next_commands]) do
-    %MarsRover{y: y, x: x, face: "N"}
+    build_rover(x, y, "N")
     |> do_send_command(next_commands)
   end
   defp do_send_command(%MarsRover{x: x, y: y, face: "N"}, ["R" | next_commands]) do
-    %MarsRover{y: y, x: x, face: "E"}
+    build_rover(x, y, "E")
     |> do_send_command(next_commands)
   end
   defp do_send_command(%MarsRover{x: x, y: y, face: "E"}, ["R" | next_commands]) do
-    %MarsRover{y: y, x: x, face: "S"}
+    build_rover(x, y, "S")
     |> do_send_command(next_commands)
   end
   defp do_send_command(%MarsRover{x: x, y: y, face: "S"}, ["R" | next_commands]) do
-    %MarsRover{y: y, x: x, face: "W"}
+    build_rover(x, y, "W")
     |> do_send_command(next_commands)
   end
+
+  defp build_rover(x, y, face), do: %MarsRover{x: x, y: y, face: face}
 end
